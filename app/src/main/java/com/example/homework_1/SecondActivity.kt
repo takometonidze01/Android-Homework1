1a4358497ea9879a46a876ecc328da679ad2d0fa
takometonidze01 <takometonidze2012@gmail.com>
2023-04-07 00:15:47 +0400

    homework 1

---
+++ app/src/main/java/com/example/homework_1/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.homework_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.homework_1.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
